2f706fed598c6298e5698b71c4190d673711126a
Maxim Volgin <maxim.volgin@me.com>
2019-01-25 20:06:01 +0100

    initial

---
+++ GitHub/LogAndSignpost.swift
@@ -0,0 +1,23 @@
+//
+//  LogAndSignpost.swift
+//  RxSpriteGame
+//
+//  Created by Maxim Volgin on 25/01/2019.
+//  Copyright © 2019 Maxim Volgin. All rights reserved.
+//
+
+import Foundation
+
+import Foundation
+import os.log
+
+struct Log {
+    fileprivate static let subsystem: String = Bundle.main.bundleIdentifier ?? ""
+    
+    static let gk = OSLog(subsystem: subsystem, category: "GK")
+    static let sk = OSLog(subsystem: subsystem, category: "SK")
+    
+}
+
+
+// os_log("touch: %@", log: Log.ui, type: .debug, "\(touch)")
---
+++ OldSchool/OldSchoolSKPhysicsContactDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  OldSchoolSKPhysicsContactDelegate.swift
+//  RxSpriteGame
+//
+//  Created by Maxim Volgin on 25/01/2019.
+//  Copyright © 2019 Maxim Volgin. All rights reserved.
+//
+
+import SpriteKit
+import os.log
+
+extension GameScene: SKPhysicsContactDelegate {
+    
+    func didBegin(_ contact: SKPhysicsContact) {
+        os_log("didBegin(_ contact: %@", log: Log.sk, type: .debug, "\(contact)")
+    }
+    
+    func didEnd(_ contact: SKPhysicsContact) {
+        os_log("didEnd(_ contact: %@", log: Log.sk, type: .debug, "\(contact)")
+    }
+    
+}
